6b49ed40833c843a06d0958304a9ba4a5abf6d5d
Hossain Muktar <HossainMuktarDev@gmail.com>
2023-12-27 04:09:56 +0600

    use jitpack.io published library on :app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,5 +66,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation(project(":my-custom-library"))
+    implementation("com.github.hossainMuktaR:CustomLibraryForAndroid:v1.0")
 }
